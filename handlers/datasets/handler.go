9a65b64634d8f77f6cb3e28cd3db278d94c93aa8
Marek <mkarwowski@equalexperts.com>
2017-01-12 14:59:48 +0000

    House Price Statistics for Small Areas removed from /datasets endpoint

--- handlers/datasets/handler.go
+++ handlers/datasets/handler.go
@@ -2,8 +2,8 @@ package datasets
 
 import (
 	"encoding/json"
-	"net/http"
 	"github.com/ONSdigital/dp-dd-api-stub/models"
+	"net/http"
 )
 
 func Handler(w http.ResponseWriter, req *http.Request) {
@@ -16,13 +16,6 @@ func Handler(w http.ResponseWriter, req *http.Request) {
 		URL:   "http://localhost:20099/datasets/AF001EW",
 		Metadata: &models.Metadata{
 			Description: "This dataset provides 2011 Census estimates that classify usual residents aged 16 and over who are members of the armed forces by residence type (household or communal resident), by sex and by age. The estimates are as at census day, 27 March 2011.",
-		},
-	}, {
-		ID:    "G39",
-		Title: "House Price Statistics for Small Areas",
-		URL:   "http://localhost:20099/datasets/G39",
-		Metadata: &models.Metadata{
-			Description: "These house price statistics are an accurate representation of the price paid for residential properties sold in a given area. They are useful for assessing the affordability of housing in small areas as well as broad patterns in prices and the number of house sales over time. Time Series Start Date: Jan-95 to Dec-95 End Date: Jan-13 to Dec-13 Periodicity Calendar Year Latest Coverage England, Wales Source Office for National Statistics Last Updated 17 February 2015",
 		},
 	}, {
 		ID:    "CPI15",
