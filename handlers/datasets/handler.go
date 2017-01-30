e35f430b9eb91e3d3634b6222a2681cd3e716bd7
Marek <mkarwowski@equalexperts.com>
2017-01-30 13:10:41 +0000

    Converting static JSON data files to source files

--- handlers/datasets/handler.go
+++ handlers/datasets/handler.go
@@ -4,14 +4,13 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/ONSdigital/dp-dd-api-stub/models"
-	"io/ioutil"
+	"github.com/ONSdigital/dp-dd-api-stub/stub"
 	"net/http"
 	"os"
-	//"strconv"
 )
 
 func Handler(w http.ResponseWriter, req *http.Request) {
-	raw, err := ioutil.ReadFile("./data/datasets.json")
+	raw, err := stub.Asset("data/datasets.json")
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
@@ -19,10 +18,6 @@ func Handler(w http.ResponseWriter, req *http.Request) {
 
 	var stubData models.Datasets = models.Datasets{}
 	json.Unmarshal(raw, &stubData)
-
-	//baseUrl := "http://" + req.Host + req.URL.EscapedPath()
-	//stubData.First = baseUrl + "?page=1"
-	//stubData.Last = baseUrl + "?page=" + strconv.Itoa(stubData.TotalPages)
 
 	jsonEncoder := json.NewEncoder(w)
 	jsonEncoder.Encode(stubData)
