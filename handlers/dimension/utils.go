1f723357e230d552ed7041bde2e9a4d0132b8ddb
Marek <mkarwowski@equalexperts.com>
2017-01-30 17:29:54 +0000

    Dimension utils, findHierarchyByDimensionID()

--- handlers/dimension/utils.go
+++ handlers/dimension/utils.go
@@ -3,6 +3,10 @@ package dimension
 import (
 	"github.com/ONSdigital/dp-dd-api-stub/models"
 	"errors"
+	"github.com/ONSdigital/dp-dd-api-stub/stub"
+	"fmt"
+	"os"
+	"encoding/json"
 )
 
 func findDatasetIndexByID(dimensions []models.Dimension, dimensionID string)  (dimension *models.Dimension, err error) {
@@ -12,4 +16,25 @@ func findDatasetIndexByID(dimensions []models.Dimension, dimensionID string)  (d
 		}
 	}
 	return &models.Dimension{}, errors.New("Dimension id " + dimensionID + " not found.")
+}
+
+func findHierarchyByDimensionID(dimensionID string) (hierarchy *models.Hierarchy, err error) {
+	var hierarchyID string = "";
+	switch dimensionID {
+	case "SP00001": hierarchyID = ""; break;
+	case "T000111": hierarchyID = ""; break;
+	}
+
+	if (hierarchyID == "") {
+		return nil, errors.New("Dimension id " + dimensionID + " not found.")
+	}
+
+	raw, err := stub.Asset("data/hierarchy/" + hierarchyID + ".json")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	json.Unmarshal(raw, &hierarchy)
+	return hierarchy, nil
 }
