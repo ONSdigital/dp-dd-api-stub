bdd3f957108d4bb4cf92aa9d508e3a2172ec6744
Marek <mkarwowski@equalexperts.com>
2017-01-30 16:24:49 +0000

    Dimension handler returns requested dimension from JSON

--- handlers/dimension/handler.go
+++ handlers/dimension/handler.go
@@ -5,73 +5,50 @@ import (
 	"net/http"
 
 	"github.com/ONSdigital/dp-dd-api-stub/models"
+	"github.com/ONSdigital/dp-dd-api-stub/stub"
+	"fmt"
+	"os"
+	"errors"
 )
 
+func findDatasetIndexByID(dimensions []models.Dimension, dimensionID string)  (dimension *models.Dimension, err error) {
+	fmt.Println(dimensions)
+	for _, dimension := range dimensions {
+		fmt.Println(dimension.ID + "==" + dimensionID)
+		if dimension.ID == dimensionID {
+			return &dimension, nil
+		}
+	}
+	return &models.Dimension{}, errors.New("Dimension id " + dimensionID + " not found.")
+}
+
 func Handler(w http.ResponseWriter, req *http.Request) {
+	datasetID := req.URL.Query().Get(":datasetId")
+	dimensionID := req.URL.Query().Get(":dimensionId")
+	raw, err := stub.Asset("data/datasets/" + datasetID + "/dimensions.json")
 
-	optionsEnglandNorthEast := make([]*models.DimensionOption, 4)
-	optionsEnglandNorthEast[0] = &models.DimensionOption{
-		ID:   "2000017",
-		Name: "Hartlepool",
-	}
-	optionsEnglandNorthEast[1] = &models.DimensionOption{
-		ID:   "2000018",
-		Name: "Middlesbrough",
-	}
-	optionsEnglandNorthEast[2] = &models.DimensionOption{
-		ID:   "2000019",
-		Name: "Redcar and Cleveland",
-	}
-	optionsEnglandNorthEast[3] = &models.DimensionOption{
-		ID:   "2000020",
-		Name: "Stockton-on-Tees",
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
 	}
 
-	optionsEngland := make([]*models.DimensionOption, 4)
-	optionsEngland[0] = &models.DimensionOption{
-		ID:      "E12000001",
-		Name:    "North East",
-		Options: optionsEnglandNorthEast,
-	}
-	optionsEngland[1] = &models.DimensionOption{
-		ID:   "E12000002",
-		Name: "North West",
-	}
-	optionsEngland[2] = &models.DimensionOption{
-		ID:   "E12000003",
-		Name: "Yorkshire and The Humber",
-	}
-	optionsEngland[3] = &models.DimensionOption{
-		ID:   "E12000004",
-		Name: "East Midlands",
-	}
+	var dimensions []models.Dimension = []models.Dimension{}
+	json.Unmarshal(raw, &dimensions)
 
-	options2 := make([]*models.DimensionOption, 2)
-	options2[0] = &models.DimensionOption{
-		ID:      "E92000001",
-		Name:    "England",
-		Options: optionsEngland,
-	}
-	options2[1] = &models.DimensionOption{
-		ID:   "W92000004",
-		Name: "Wales",
-	}
-
-	options := make([]*models.DimensionOption, 1)
-	options[0] = &models.DimensionOption{
-		ID:      "K04000001",
-		Name:    "England and Wales",
-		Options: options2,
-	}
-
-	dimension := &models.Dimension{
-		ID:      "D000123",
-		Name:    "Geography",
-		Options: options,
+	dimension, err:= findDatasetIndexByID(dimensions, dimensionID)
+	if err != nil {
+		fmt.Println(err)
+		response, err := json.Marshal(models.Dimension{})
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.WriteHeader(400)
+		w.Write(response)
+		return
 	}
 
 	jsonEncoder := json.NewEncoder(w)
 	jsonEncoder.Encode(dimension)
-
 	w.WriteHeader(200)
 }
